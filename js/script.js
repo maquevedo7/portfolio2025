6d49f8a55c06b1bd2173afe53817bb500115e1ec
Hiromi Acosta Quevedo <54210135+maquevedo7@users.noreply.github.com>
2025-03-03 10:15:22 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* Typing animation */
+
+var typed = new Typed(".typing", {
+    strings: [
+        "",
+        "Digital Arts Engineer",
+        "Frontend Developer",
+        "Multimedia Creator",
+    ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+});
